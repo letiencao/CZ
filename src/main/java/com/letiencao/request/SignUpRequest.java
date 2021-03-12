ebb4c5137bc2c69e52d430df5b890e736eb67ca2
Le Tien Cao <caole28072000@gmail.com>
2021-03-12 20:08:44 +0700

    Add SignUpRequest.java

---
+++ src/main/java/com/letiencao/request/SignUpRequest.java
@@ -0,0 +1,28 @@
+package com.letiencao.request;
+
+public class SignUpRequest {
+	private String phoneNumber;
+	private String password;
+	private String uuid;
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	
+	
+
+}
