cb3a4e75568905136807c263850a5342b252134e
Le Tien Cao <caole28072000@gmail.com>
2021-03-12 20:02:59 +0700

    update SignUpAPI

--- src/main/java/com/letiencao/api/SignUpAPI.java
+++ src/main/java/com/letiencao/api/SignUpAPI.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.letiencao.model.AccountModel;
+import com.letiencao.request.SignUpRequest;
 import com.letiencao.response.SignUpResponse;
 import com.letiencao.service.IAccountService;
 import com.letiencao.service.impl.AccountService;
@@ -34,31 +35,63 @@ public class SignUpAPI extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("application/json");
 		Gson gson = new  Gson();
-		AccountModel accountModel = gson.fromJson(request.getReader(), AccountModel.class);
+//		AccountModel accountModel = gson.fromJson(request.getReader(), AccountModel.class);
+//		SignUpResponse signUpResponse = new SignUpResponse();
+//		String phoneNumber = accountModel.getPhoneNumber();
+//		String password = accountModel.getPassword();
+//		AccountModel model = null;
+//		Pattern p = Pattern.compile("[^A-Za-z0-9]");
+//		Matcher m = p.matcher(password);
+//		if(!m.find() && phoneNumber.length() == 10 && phoneNumber.charAt(0) == '0' && password.length()>=6 && password.length() <=10 && !phoneNumber.equalsIgnoreCase(password)) {
+//			model = accountService.signUp(accountModel);
+//			if(model != null) {
+//				signUpResponse.setCode(1000);
+//				signUpResponse.setMessage("OK");
+//				signUpResponse.setAccountModel(model);
+//			}else {
+//				signUpResponse.setCode(9996);
+//				signUpResponse.setMessage("User existed");
+//				signUpResponse.setAccountModel(null);
+//			}
+//		}else {
+//			signUpResponse.setCode(1004);
+//			signUpResponse.setMessage("Parameter value is invalid");
+//			signUpResponse.setAccountModel(null);
+//		}
+//		
+//		response.getWriter().print(gson.toJson(signUpResponse));
 		SignUpResponse signUpResponse = new SignUpResponse();
-		String phoneNumber = accountModel.getPhoneNumber();
-		String password = accountModel.getPassword();
-		AccountModel model = null;
+		SignUpRequest signUpRequest = gson.fromJson(request.getReader(), SignUpRequest.class);
+		String phoneNumber = signUpRequest.getPhoneNumber();
+		String password = signUpRequest.getPassword();
+//		@SuppressWarnings("unused")
+		String uuid = signUpRequest.getUuid();
 		Pattern p = Pattern.compile("[^A-Za-z0-9]");
 		Matcher m = p.matcher(password);
-		if(!m.find() && phoneNumber.length() == 10 && phoneNumber.charAt(0) == '0' && password.length()>=6 && password.length() <=10 && !phoneNumber.equalsIgnoreCase(password)) {
-			model = accountService.signUp(accountModel);
-			if(model != null) {
-				signUpResponse.setCode(1000);
-				signUpResponse.setMessage("OK");
-				signUpResponse.setAccountModel(model);
+		AccountModel accountModel = new AccountModel();
+		if(phoneNumber == "" || password == "" || uuid == "") {
+			signUpResponse.setCode(1002);
+			signUpResponse.setMessage("Parameter is not enough");
+			signUpResponse.setAccountModel(null);
+		}else {
+			if(!m.find() && phoneNumber.length() == 10 && phoneNumber.charAt(0) == '0' && password.length()>=6 && password.length() <=10 && !phoneNumber.equalsIgnoreCase(password)) {
+				accountModel = accountService.signUp(signUpRequest);
+				if(accountModel != null) {
+					signUpResponse.setCode(1000);
+					signUpResponse.setMessage("OK");
+					signUpResponse.setAccountModel(accountModel);
+				}else {
+					signUpResponse.setCode(9996);
+					signUpResponse.setMessage("User existed");
+					signUpResponse.setAccountModel(null);
+				}
 			}else {
-				signUpResponse.setCode(9996);
-				signUpResponse.setMessage("User existed");
+				signUpResponse.setCode(1004);
+				signUpResponse.setMessage("Parameter value is invalid");
 				signUpResponse.setAccountModel(null);
 			}
-		}else {
-			signUpResponse.setCode(1004);
-			signUpResponse.setMessage("Parameter value is invalid");
-			signUpResponse.setAccountModel(null);
 		}
-		
-		response.getWriter().print(gson.toJson(signUpResponse));
+		response.getWriter().print(gson.toJson(signUpResponse));;
 	}
 
 }
