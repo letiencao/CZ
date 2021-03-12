abedf4ceed77316ecd3dda6cb2a184a0525e67d5
Le Tien Cao <caole28072000@gmail.com>
2021-03-12 20:09:45 +0700

    Update IAccountService.java

--- src/main/java/com/letiencao/service/IAccountService.java
+++ src/main/java/com/letiencao/service/IAccountService.java
@@ -3,9 +3,11 @@ package com.letiencao.service;
 import com.letiencao.model.AccountModel;
 import com.letiencao.request.PhoneNumberRequest;
 import com.letiencao.request.SignInRequest;
+import com.letiencao.request.SignUpRequest;
+import com.letiencao.response.SignUpResponse;
 
 public interface IAccountService {
-	AccountModel signUp(AccountModel accountModel);
+	AccountModel signUp(SignUpRequest signUpRequest);
 	String signIn(SignInRequest signInRequest);
 	AccountModel findByPhoneNumber(PhoneNumberRequest phoneNumberRequest);
 }
