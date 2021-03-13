71ad47920778bdd6e443e94f6f6439c79de8f763
Le Tien Cao <caole28072000@gmail.com>
2021-03-14 01:14:45 +0700

    update GenericDAO.java

--- src/main/java/com/letiencao/dao/GenericDAO.java
+++ src/main/java/com/letiencao/dao/GenericDAO.java
@@ -6,7 +6,7 @@ import com.letiencao.mapping.IRowMapping;
 
 public interface GenericDAO<T> {
 	List<T> findAll(String sql,IRowMapping<T> rowMapping);
-	boolean insertOne(String sql,Object... parameters );
+	Long insertOne(String sql,Object... parameters );
 	boolean update (String sql,Object... parameters);
 	T findOne(String sql,IRowMapping<T> rowMapping,Object... parameters);
 
