8ad9f4a87f1938ba37067d5c65e470d1176bdd12
Le Tien Cao <caole28072000@gmail.com>
2021-03-14 01:26:34 +0700

    add DataPostResponse.java

---
+++ src/main/java/com/letiencao/response/DataPostResponse.java
@@ -0,0 +1,20 @@
+package com.letiencao.response;
+
+public class DataPostResponse {
+	private Long id;
+	private String url;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+
+}
