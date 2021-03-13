ecb41f6b47f29b86d6d694ef613ce7a377f4762d
Le Tien Cao <caole28072000@gmail.com>
2021-03-14 01:30:35 +0700

    add FileService.java

---
+++ src/main/java/com/letiencao/service/impl/FileService.java
@@ -0,0 +1,29 @@
+package com.letiencao.service.impl;
+
+import java.sql.Timestamp;
+
+import com.letiencao.dao.IFileDAO;
+import com.letiencao.dao.impl.FileDAO;
+import com.letiencao.model.FileModel;
+import com.letiencao.service.IFileService;
+
+public class FileService implements IFileService {
+	private IFileDAO fileDAO;
+	public FileService() {
+		fileDAO = new FileDAO();
+	}
+
+	@Override
+	public FileModel insertOne(FileModel fileModel) {
+		fileModel.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+		fileModel.setDeleted(false);
+		return fileDAO.insertOne(fileModel);
+	}
+
+	@Override
+	public FileModel findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
