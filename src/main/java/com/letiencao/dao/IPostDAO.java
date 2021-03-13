0d647fcab0f920408fbcea78940206c493708e72
Le Tien Cao <caole28072000@gmail.com>
2021-03-14 01:16:06 +0700

    update IPostDAO.java

--- src/main/java/com/letiencao/dao/IPostDAO.java
+++ src/main/java/com/letiencao/dao/IPostDAO.java
@@ -1,8 +1,12 @@
 package com.letiencao.dao;
 
+import java.util.List;
+
 import com.letiencao.model.PostModel;
 
 public interface IPostDAO {
+	List<PostModel> findAll();
 	PostModel insertOne(PostModel postModel);
+	PostModel findPostById(Long id);
 
 }
