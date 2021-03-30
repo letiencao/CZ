c9b3f327792c2a4680fd9aabc8e1b5b31d36b368
Le Tien Cao <caole28072000@gmail.com>
2021-03-30 22:52:49 +0700

    add GetListPostResponse

--- src/main/java/com/letiencao/response/post/GetListPostResponse.java
+++ src/main/java/com/letiencao/response/post/GetListPostResponse.java
@@ -1,19 +1,36 @@
 package com.letiencao.response.post;
 
-public class DataGetListPostResponse extends DataGetPostReponse {
+import java.util.List;
+
+import com.letiencao.response.BaseResponse;
+
+public class GetListPostResponse extends BaseResponse {
+	private List<DataGetPostReponse> posts;
 	private int new_items;
 	private Long last_id;
+
 	public int getNew_items() {
 		return new_items;
 	}
+
 	public void setNew_items(int new_items) {
 		this.new_items = new_items;
 	}
+
 	public Long getLast_id() {
 		return last_id;
 	}
+
 	public void setLast_id(Long last_id) {
 		this.last_id = last_id;
 	}
-	
+
+	public List<DataGetPostReponse> getList() {
+		return posts;
+	}
+
+	public void setList(List<DataGetPostReponse> list) {
+		this.posts = list;
+	}
+
 }
