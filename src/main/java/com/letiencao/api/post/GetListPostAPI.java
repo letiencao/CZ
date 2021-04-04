1a2b0f30677643101c410fca725e35e0c9d58a11
Le Tien Cao <caole28072000@gmail.com>
2021-04-04 10:21:32 +0700

    Update GetListPostAPI

--- src/main/java/com/letiencao/api/post/GetListPostAPI.java
+++ src/main/java/com/letiencao/api/post/GetListPostAPI.java
@@ -63,6 +63,7 @@ public class GetListPostAPI extends HttpServlet {
 		fileService = new FileService();
 	}
 
+	@SuppressWarnings("static-access")
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
